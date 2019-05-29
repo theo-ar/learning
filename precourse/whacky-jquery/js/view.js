63f47dd895668d65e087f184b65a9bf846caaf49
Theo Macris <theodoulos.argyris@gmail.com>
2019-05-29 20:05:35 +0100

    Exercises completed

--- precourse/whacky-jquery/js/view.js
+++ precourse/whacky-jquery/js/view.js
@@ -23,6 +23,7 @@ View.prototype.exercise1 = function () {
   //Result: Every square should turn orange (we already have a CSS rule)
 
   //your code here!
+  $('.square').addClass("orange");
 };
 
 View.prototype.exercise2 = function () {
@@ -30,6 +31,7 @@ View.prototype.exercise2 = function () {
   //Result: Every square vanishes
 
   //your code here!
+  $("ul").children().remove()
 };
 
 View.prototype.exercise3 = function () {
@@ -37,6 +39,8 @@ View.prototype.exercise3 = function () {
   //Result: An <h1> with the text 'i love jquery' appears under the grid.
 
   //your code here!
+  $("ul").parent().after("<h1>i love jquery</h1>");
+  $("h1").css("text-align", "center");
 };
 
 View.prototype.exercise4 = function () {
@@ -44,6 +48,8 @@ View.prototype.exercise4 = function () {
   //Result: Your name appears in every other square.
 
   //your code here!
+  $(".square:nth-child(even)").text("TM");
+  
 };
 
 View.prototype.exercise5 = function () {
@@ -55,6 +61,11 @@ View.prototype.exercise5 = function () {
   //  'data-pos' of every square
 
   //your code here!
+  $('.square').on("click", e => {
+    const $sq = $(e.currentTarget);
+    alert($sq.attr("data-pos"));
+  })
+
 };
 
 View.prototype.exercise6 = function () {
@@ -65,6 +76,9 @@ View.prototype.exercise6 = function () {
   //hint: use window._randomColorString() (defined at top) to get a random color!
 
   //your code here!
+  $('.square').each(function(i, el){
+    $(el).css("background-color", window._randomColorString());
+  })
 };
 
 View.prototype.exercise7 = function(){
@@ -75,6 +89,10 @@ View.prototype.exercise7 = function(){
   //rainbow.
 
   //your code here!
+  $('.square').mouseover(e => {
+    const $sq = $(e.currentTarget);
+    alert($sq.css("background-color"))
+  })
 };
 
 
